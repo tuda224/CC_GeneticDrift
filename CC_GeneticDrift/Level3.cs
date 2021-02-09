16dbabda221bcce1b1ec517c6e28ec33a6731603
tuda224 <johann.tuder@gmail.com>
2021-02-09 20:25:39 +0100

    initial commit

---
+++ CC_GeneticDrift/Level3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CC_GeneticDrift
+{
+    public class Level3
+    {
+        public static void Run()
+        {
+            var input = ReadInput();
+            var splittedInput = input.Split();
+        }
+
+        private static string ReadInput()
+        {
+            Console.WriteLine("Provide path to input file:");
+            var filePath = Console.ReadLine();
+            var input = File.ReadAllText(filePath);
+            return input;
+        }
+    }
+}
---
+++ CC_GeneticDrift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CC_GeneticDrift
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Level1.Run();
+                //Level2.Run();
+                //Level3.Run();
+                //Level4.Run();
+            }
+        }
+    }
+}
